74680c910e785f92c207cc4f03768a3ee2e13ff7
Thutmose <elpatricimo@gmail.com>
2021-01-11 15:42:35 -0500

    add these properly now via junction instead of soft link

--- docs/source/pokemobs/links.ps1
+++ docs/source/pokemobs/links.ps1
@@ -1,1 +1,1 @@
-New-Item -ItemType SymbolicLink -Force -Name ".\subpages" -Target "E:\git\Pokecube-Development\1.16.x\Pokecube-AIO-1.16.x\run\config\pokecube\pokemobs\subpages"
+New-Item -ItemType Junction -Path ".\subpages" -Target "E:\git\Pokecube-Development\1.16.x\Pokecube-AIO-1.16.x\run\config\pokecube\pokemobs\subpages"
