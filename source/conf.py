0c6920f57d40e8e937d4703ece1186b77aaff2af
WenXin2 <dwnewville@gmail.com>
2023-10-12 21:26:24 -0500

    Fix theme?

--- source/conf.py
+++ source/conf.py
@@ -53,17 +53,14 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'default'
+html_theme = 'default'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_style = 'default'
-
 html_css_files = [
-    'css/theme.css',
     'default.css',
     'red.css'
 ]
