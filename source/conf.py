eaac880fcbaf5a58c48be04e2dc3a0c2d4603487
WenXin2 <dwnewville@gmail.com>
2023-10-12 23:56:55 -0500

    Fix theme?

--- source/conf.py
+++ source/conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
+# import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -52,8 +52,8 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = os.environ.get("SPHINX_THEME", "sphinx_rtd_theme")
+
+html_theme = default
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
